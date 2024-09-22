dd254093698b100f8ee43b8470776414138d921e
eduard-balamatiuc <balamatiuc2@gmail.com>
2024-09-22 16:33:20 +0300

    solve tox

--- src/hrms_back/config.py
+++ src/hrms_back/config.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_env_or_raise(key: str) -> str:
     """Get an environment variable or raise an exception."""
     value = os.getenv(key)
     if value is None:
         raise ValueError(f"Environment variable {key} is not set")
     return value
+
 
 POSTGRES_HOST = get_env_or_raise("POSTGRES_HOST")
 POSTGRES_PORT = get_env_or_raise("POSTGRES_PORT")
