c93d9a0f7588dea24d7e28fb3030a33a9f5ff707
russian17 <simion.cuzmin@isa.utm.md>
2024-10-03 15:55:14 +0300

    added version alembic file

---
+++ src/hrms_back/migrations/versions/22da65ef65ca_add_status_column_to_appointment_table.py
@@ -0,0 +1,26 @@
+"""Add status column to appointment table
+
+Revision ID: 22da65ef65ca
+Revises: e4f1a07ed6c7
+Create Date: 2024-10-03 12:50:23.236874
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '22da65ef65ca'
+down_revision = 'e4f1a07ed6c7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Add the 'status' column to the 'appointment' table
+    op.add_column('appointment', sa.Column('status', sa.String(), nullable=False))
+
+def downgrade():
+    # Remove the 'status' column if the migration is downgraded
+    op.drop_column('appointment', 'status')
+
